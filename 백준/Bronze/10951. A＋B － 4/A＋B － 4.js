3075603decd78539539adfaff8120fdfd8107310
4mafreesia <37726487+LenaKwon@users.noreply.github.com>
2023-09-09 02:11:17 +1200

    [Bronze V] Title: A+B - 4, Time: 128 ms, Memory: 9572 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -0,0 +1,6 @@
+let input = require("fs").readFileSync("dev/stdin").toString().trim().split("\n");
+
+for(let i=0; i< input.length; i++){
+    let nums = input[i].split(" ").map(val=>+val);
+    console.log(nums[0]+nums[1]);   
+}
