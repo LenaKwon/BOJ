d00f96b92a4e273077867c6a364cd0418836fe28
lenn-dev <37726487+lenn-dev@users.noreply.github.com>
2023-12-20 11:04:54 +1300

    [Bronze III] Title: 오븐 시계, Time: 124 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.js
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.js
@@ -3,11 +3,8 @@ const time =fs[0].split(" ").map(val=>+val);
 const cookTime=Number(fs[1]);
 const [hr,min]=time;
 
-const h = Math.floor((min+cookTime)/60)
-const m = (min+cookTime)%60
-
 console.log (
-  h < 1 ? hr + ' ' + (min + cookTime)
-  : hr + h > 23 ? hr + h - 24 + " " + m
-  : hr + h + " " + m
+  (min+cookTime)/60 < 1 ? hr + ' ' + (min + cookTime)
+  : hr + Math.floor((min+cookTime)/60) > 23 ? hr + Math.floor((min+cookTime)/60)-24 + " " + (min+cookTime)%60
+  : hr + Math.floor((min+cookTime)/60) + " " + (min+cookTime)%60
 )
